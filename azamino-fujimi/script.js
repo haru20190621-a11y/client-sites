5a6fde72572a2195d5114da824f065916bc0415d
haru20190621-a11y <haru20190621@gmail.com>
2026-08-15 05:12:06 +0900

    add azamino-fujimi

---
+++ azamino-fujimi/script.js
@@ -0,0 +1,24 @@
+window.__revealReady = true;
+(function(){
+  var els = document.querySelectorAll('.reveal');
+  var scrolled = false;
+  function check(){
+    var vh = window.innerHeight;
+    Array.prototype.forEach.call(els, function(el){
+      if(!el.classList.contains('on') && el.getBoundingClientRect().top < vh * 0.9)
+        el.classList.add('on');
+    });
+  }
+  window.addEventListener('scroll', function(){ scrolled = true; check(); }, {passive:true});
+  window.addEventListener('load', check);
+  var t = setInterval(function(){
+    check();
+    if(!document.querySelector('.reveal:not(.on)')) clearInterval(t);
+  }, 700);
+  // フェイルセーフ: scrollイベントが一度も飛んでこない環境では演出を捨てて全部表示する
+  setTimeout(function(){
+    if(!scrolled && window.scrollY === 0)
+      Array.prototype.forEach.call(els, function(el){ el.classList.add('on'); });
+  }, 5000);
+  check();
+})();
